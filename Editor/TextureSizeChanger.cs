c487e518ab7ed94a4c7ae635f198b911415a8268
Emiro <75586257+EmiroDeve@users.noreply.github.com>
2025-03-15 12:10:15 +0330

    Add files via upload

--- Editor/TextureSizeChanger.cs
+++ Editor/TextureSizeChanger.cs
@@ -2,85 +2,138 @@ using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
 
-public class TextureSizeChanger : EditorWindow
+namespace YourName.TextureSizeChanger
 {
-    private enum TextureSize { _32 = 32, _64 = 64, _128 = 128, _256 = 256, _512 = 512, _1024 = 1024, _2048 = 2048, _4096 = 4096, _8192 = 8192, _16384 = 16384 }
-    private TextureSize selectedSize = TextureSize._256;
-
-    private List<Texture2D> excludeTextures = new List<Texture2D>();
-
-    [MenuItem("Tools/Texture Size Changer")]
-    public static void ShowWindow()
+    public class TextureSizeChanger : EditorWindow
     {
-        GetWindow<TextureSizeChanger>("Texture Size Changer");
-    }
+        private enum TextureSize { _32 = 32, _64 = 64, _128 = 128, _256 = 256, _512 = 512, _1024 = 1024, _2048 = 2048, _4096 = 4096, _8192 = 8192, _16384 = 16384 }
+        private TextureSize selectedSize = TextureSize._256;
+        private List<Texture2D> excludeTextures = new List<Texture2D>();
+        private const string SizePrefKey = "TextureSizeChanger_SelectedSize";
+        private const string ExcludeTexturesPrefKey = "TextureSizeChanger_ExcludeTextures";
 
-    private void OnGUI()
-    {
-        GUILayout.Label("Select Max Texture Size", EditorStyles.boldLabel);
-        selectedSize = (TextureSize)EditorGUILayout.EnumPopup("Max Size", selectedSize);
-
-        GUILayout.Space(10);
-        GUILayout.Label("Exclude Textures", EditorStyles.boldLabel);
-
-        for (int i = 0; i < excludeTextures.Count; i++)
+        [MenuItem("Tools/Texture Size Changer")]
+        public static void ShowWindow()
         {
-            GUILayout.BeginHorizontal();
-            excludeTextures[i] = (Texture2D)EditorGUILayout.ObjectField(excludeTextures[i], typeof(Texture2D), false);
+            GetWindow<TextureSizeChanger>("Texture Size Changer");
+        }
 
-            if (GUILayout.Button("X", GUILayout.Width(20)))
+        private void OnEnable()
+        {
+            LoadPreferences();
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("Select Max Texture Size", EditorStyles.boldLabel);
+            selectedSize = (TextureSize)EditorGUILayout.EnumPopup("Max Size", selectedSize);
+
+            GUILayout.Space(10);
+            GUILayout.Label("Exclude Textures", EditorStyles.boldLabel);
+
+            for (int i = 0; i < excludeTextures.Count; i++)
             {
-                excludeTextures.RemoveAt(i);
-            }
-            GUILayout.EndHorizontal();
-        }
+                GUILayout.BeginHorizontal();
+                excludeTextures[i] = (Texture2D)EditorGUILayout.ObjectField(excludeTextures[i], typeof(Texture2D), false);
 
-        if (GUILayout.Button("Add Texture"))
-        {
-            excludeTextures.Add(null);
-        }
-
-        GUILayout.Space(10);
-        if (GUILayout.Button("Apply to All Textures"))
-        {
-            ChangeTextureSizes((int)selectedSize);
-        }
-    }
-
-    private void ChangeTextureSizes(int newSize)
-    {
-        string[] guids = AssetDatabase.FindAssets("t:Texture");
-
-        HashSet<string> excludePaths = new HashSet<string>();
-        foreach (var tex in excludeTextures)
-        {
-            if (tex != null)
-            {
-                string path = AssetDatabase.GetAssetPath(tex);
-                excludePaths.Add(path);
-            }
-        }
-
-        foreach (string guid in guids)
-        {
-            string path = AssetDatabase.GUIDToAssetPath(guid);
-
-            if (excludePaths.Contains(path))
-            {
-                Debug.Log($"Skipping: {path}");
-                continue;
+                if (GUILayout.Button("X", GUILayout.Width(20)))
+                {
+                    excludeTextures.RemoveAt(i);
+                }
+                GUILayout.EndHorizontal();
             }
 
-            TextureImporter importer = AssetImporter.GetAtPath(path) as TextureImporter;
-
-            if (importer != null)
+            if (GUILayout.Button("Add Texture"))
             {
-                importer.maxTextureSize = newSize;
-                AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
+                excludeTextures.Add(null);
             }
+
+            GUILayout.Space(10);
+            if (GUILayout.Button("Apply to All Textures"))
+            {
+                ChangeTextureSizes((int)selectedSize);
+            }
+
+            SavePreferences();
         }
 
-        Debug.Log($"All textures' max size changed to {newSize}, except excluded ones.");
-        AssetDatabase.SaveAssets();
+        private void ChangeTextureSizes(int newSize)
+        {
+            string[] guids = AssetDatabase.FindAssets("t:Texture");
+
+            HashSet<string> excludePaths = new HashSet<string>();
+            foreach (var tex in excludeTextures)
+            {
+                if (tex != null)
+                {
+                    string path = AssetDatabase.GetAssetPath(tex);
+                    excludePaths.Add(path);
+                }
+            }
+
+            foreach (string guid in guids)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guid);
+
+                if (excludePaths.Contains(path))
+                {
+                    Debug.Log($"Skipping: {path}");
+                    continue;
+                }
+
+                TextureImporter importer = AssetImporter.GetAtPath(path) as TextureImporter;
+
+                if (importer != null)
+                {
+                    importer.maxTextureSize = newSize;
+                    AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
+                }
+            }
+
+            Debug.Log($"All textures' max size changed to {newSize}, except excluded ones.");
+            AssetDatabase.SaveAssets();
+        }
+
+        private void SavePreferences()
+        {
+            EditorPrefs.SetInt(SizePrefKey, (int)selectedSize);
+
+            List<string> excludePaths = new List<string>();
+            foreach (var tex in excludeTextures)
+            {
+                if (tex != null)
+                {
+                    excludePaths.Add(AssetDatabase.GetAssetPath(tex));
+                }
+            }
+            EditorPrefs.SetString(ExcludeTexturesPrefKey, string.Join(",", excludePaths));
+        }
+
+        private void LoadPreferences()
+        {
+            if (EditorPrefs.HasKey(SizePrefKey))
+            {
+                selectedSize = (TextureSize)EditorPrefs.GetInt(SizePrefKey);
+            }
+
+            if (EditorPrefs.HasKey(ExcludeTexturesPrefKey))
+            {
+                string savedPaths = EditorPrefs.GetString(ExcludeTexturesPrefKey);
+                string[] paths = savedPaths.Split(',');
+
+                excludeTextures.Clear();
+                foreach (var path in paths)
+                {
+                    if (!string.IsNullOrEmpty(path))
+                    {
+                        Texture2D texture = AssetDatabase.LoadAssetAtPath<Texture2D>(path);
+                        if (texture != null)
+                        {
+                            excludeTextures.Add(texture);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
